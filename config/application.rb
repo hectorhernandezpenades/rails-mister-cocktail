71322fd0e9f4e2ce3f8428905f5cb5f60a45f473
Hector Hernandez <hectorhernandezpenades@gmail.com>
2019-04-03 02:26:40 +0100

    config.assets.precompile

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module RailsMisterCocktail
     # Initialize configuration defaults for originally generated Rails version.
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.load_defaults 5.2
-    config.assets.precompile = ['*.js', '**/*.js']
+    config.assets.precompile = ['*.erb', '**/*.erb']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
